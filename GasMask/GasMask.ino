b8aeb6fb006ed076d39d815880c38eec4ce92420
Zach Wasserman <zach@fleetdm.com>
2022-07-31 22:28:58 -0700

    Initial commit for GasMask

---
+++ GasMask/GasMask.ino
@@ -0,0 +1,197 @@
+#include <FastLED.h>
+
+#include "palettes.h"
+
+#define LED_PIN     2
+#define NUM_LEDS    8
+#define BRIGHTNESS  128
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 80
+
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+extern CRGBPalette16 redPurpleBlue;
+extern const TProgmemPalette16 redPurpleBlue_p PROGMEM;
+
+void setup() {
+    delay( 200 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    
+    currentPalette = ib04_gp;
+    //SetupPurpleAndRedPalette();
+    currentBlending = LINEARBLEND;
+}
+
+
+void loop()
+{
+    ChangePalettePeriodically();
+    
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
+    
+    FillLEDsFromPaletteColors( startIndex);
+    
+    FastLED.show();
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; ++i) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
+
+
+// There are several different palettes of colors demonstrated here.
+//
+// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
+// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+//
+// Additionally, you can manually define your own color palettes, or you can write
+// code that creates color palettes on the fly.  All are shown here.
+
+void ChangePalettePeriodically()
+{
+    uint8_t secondHand = ((millis() / 1000) / 5) % 60;
+    static uint8_t lastSecond = 99;
+    
+    if( lastSecond != secondHand) {
+        lastSecond = secondHand;
+        if( secondHand ==  0)  { currentPalette = hangonfatboy_gp;         currentBlending = LINEARBLEND; }
+        if( secondHand == 10)  { currentPalette = hangonfatboy_gp;   currentBlending = NOBLEND;  }
+        if( secondHand == 15)  { currentPalette = rgi_15_gp;   currentBlending = LINEARBLEND; }
+        if( secondHand == 20)  { SetupPurpleAndRedPalette();               currentBlending = LINEARBLEND; }
+        if( secondHand == 25)  { currentPalette = bhw1_19_gp;              currentBlending = LINEARBLEND; }
+        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
+        if( secondHand == 35)  { currentPalette = bhw3_07_gp;       currentBlending = LINEARBLEND; }
+        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
+        if( secondHand == 55)  { currentPalette = ib04_gp; currentBlending = LINEARBLEND; }
+    }
+}
+
+// This function fills the palette with totally random colors.
+void SetupTotallyRandomPalette()
+{
+    for( int i = 0; i < 16; ++i) {
+        currentPalette[i] = CHSV( random8(), 255, random8());
+    }
+}
+
+// This function sets up a palette of black and white stripes,
+// using code.  Since the palette is effectively an array of
+// sixteen CRGB colors, the various fill_* functions can be used
+// to set them up.
+void SetupBlackAndWhiteStripedPalette()
+{
+    // 'black out' all 16 palette entries...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    // and set every fourth one to white.
+    currentPalette[0] = CRGB::White;
+    currentPalette[4] = CRGB::White;
+    currentPalette[8] = CRGB::White;
+    currentPalette[12] = CRGB::White;
+    
+}
+
+// This function sets up a palette of purple and green stripes.
+void SetupPurpleAndRedPalette()
+{
+    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+    CRGB red  = CHSV( HUE_RED, 255, 255);
+    CRGB black  = CRGB::Black;
+    
+    currentPalette = CRGBPalette16(
+                                   red,  red,  black,  black,
+                                   purple, purple, black,  black,
+                                   red,  red,  black,  black,
+                                   purple, purple, black,  black );
+}
+
+
+// This example shows how to set up a static color palette
+// which is stored in PROGMEM (flash), which is almost always more
+// plentiful than RAM.  A static PROGMEM palette like this
+// takes up 64 bytes of flash.
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Gray, // 'white' is too bright compared to red and blue
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
+
+const TProgmemPalette16 redPurpleBlue_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Purple, // 'white' is too bright compared to red and blue
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Purple,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Purple,
+    CRGB::Purple,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
+
+
+
+
+// Additional notes on FastLED compact palettes:
+//
+// Normally, in computer graphics, the palette (or "color lookup table")
+// has 256 entries, each containing a specific 24-bit RGB color.  You can then
+// index into the color palette using a simple 8-bit (one byte) value.
+// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
+// is quite possibly "too many" bytes.
+//
+// FastLED does offer traditional 256-element palettes, for setups that
+// can afford the 768-byte cost in RAM.
+//
+// However, FastLED also offers a compact alternative.  FastLED offers
+// palettes that store 16 distinct entries, but can be accessed AS IF
+// they actually have 256 entries; this is accomplished by interpolating
+// between the 16 explicit entries to create fifteen intermediate palette
+// entries between each pair.
+//
+// So for example, if you set the first two explicit entries of a compact 
+// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
+// the first sixteen entries from the virtual palette (of 256), you'd get
+// Green, followed by a smooth gradient from green-to-blue, and then Blue.
---
+++ GasMask/palettes.h
@@ -0,0 +1,80 @@
+// Gradient palette "hangonfatboy_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/hangonfatboy.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+DEFINE_GRADIENT_PALETTE( hangonfatboy_gp ) {
+    0, 215, 74,  6,
+   30,  74, 22, 53,
+   61,  18,  6, 27,
+   96,   5, 36, 85,
+  114,   1,  8, 30,
+  140,   1,  8, 30,
+  158,   5, 36, 85,
+  193,  18,  6, 27,
+  224,  74, 22, 53,
+  255, 215, 74,  6};
+
+// Gradient palette "rgi_15_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/ds/rgi/tn/rgi_15.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 36 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( rgi_15_gp ) {
+    0,   4,  1, 31,
+   31,  55,  1, 16,
+   63, 197,  3,  7,
+   95,  59,  2, 17,
+  127,   6,  2, 34,
+  159,  39,  6, 33,
+  191, 112, 13, 32,
+  223,  56,  9, 35,
+  255,  22,  6, 38};
+
+// Gradient palette "bhw1_19_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_19.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw1_19_gp ) {
+    0,  82,  1,  3,
+   79,  20,  1,  1,
+  181, 139,  1,  0,
+  252,  20,  1,  1,
+  255,  20,  1,  1};
+
+// Gradient palette "bhw3_07_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw3/tn/bhw3_07.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw3_07_gp ) {
+    0, 132,  5,  1,
+   30,  12, 13,147,
+   53,  54, 32,166,
+   73, 103, 39, 98,
+   84, 239, 52, 27,
+   96,  71,  2,  1,
+  117,  46,  8, 33,
+  130,  54, 32,166,
+  163,  10,  7, 78,
+  196, 239, 52, 27,
+  255,   1,  1, 14};
+
+// Gradient palette "ib04_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/ing/general/tn/ib04.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 48 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( ib04_gp ) {
+    0,  31,  2,  1,
+   20,  82,  5,  2,
+   40,  42,  2,  1,
+   76, 249, 39, 30,
+  109,  27,  1,  1,
+  142,  54,  3,  2,
+  158, 135, 10,  6,
+  178, 186, 13,  7,
+  201, 139, 19, 12,
+  219, 249, 86, 67,
+  239,  63,  2,  1,
+  255,   0,  0,  0};
